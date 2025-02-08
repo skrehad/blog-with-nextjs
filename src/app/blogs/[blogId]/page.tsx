55a3f194c838f885896791513a5af19bdbd394d4
rehadhasan <rehadhasan664@gmail.com>
2025-02-08 23:42:59 +0600

    create dynamic title

--- src/app/blogs/[blogId]/page.tsx
+++ src/app/blogs/[blogId]/page.tsx
@@ -1,5 +1,22 @@
 import BlogDetailsCard from "@/components/ui/BlogDetailsCard";
 
+// for showing title in tab
+export async function generateMetadata({
+  params,
+}: {
+  params: Promise<{ blogId: string }>;
+}) {
+  const { blogId } = await params;
+
+  const res = await fetch(`http://localhost:5000/blogs/${blogId}`);
+  const blog = await res.json();
+
+  return {
+    title: blog.title,
+    description: blog.description,
+  };
+}
+
 const BlogDetailsPage = async ({
   params,
 }: {

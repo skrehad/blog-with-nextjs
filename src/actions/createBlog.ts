04ffda63dd61e7e96f37a8113e380c628100fd27
rehadhasan <rehadhasan664@gmail.com>
2025-02-08 21:26:07 +0600

    blog create successfully

--- src/actions/createBlog.ts
+++ src/actions/createBlog.ts
@@ -1,15 +1,22 @@
 "use server";
 
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-export const createBlog = async (data: any) => {
+import { redirect } from "next/navigation";
+
+export const createBlog = async (data: FormData) => {
+  const blogData = Object.fromEntries(data.entries());
+
   const res = await fetch("http://localhost:5000/blogs", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
     },
-    body: JSON.stringify(data),
+    body: JSON.stringify(blogData),
   });
 
   const blogInfo = await res.json();
+
+  if (blogInfo) {
+    redirect(`/blogs/${blogInfo.id}`);
+  }
   return blogInfo;
 };
